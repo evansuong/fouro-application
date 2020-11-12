8075678f2970b00575f1a6bb600adb3087942158
terry <tzfeng@ucsd.edu>
2020-11-12 08:38:54 -0800

    Test addUser

--- fouro-application/backend/users/users.js
+++ fouro-application/backend/users/users.js
@@ -1,4 +1,18 @@
 import "firebase/firestore";
 import "firebase/auth";
 
-import db from "../firebase/config";
+import Fire from "../firebase/config";
+
+// Users Class for Creating and Updating Users
+export default class Users {
+  db = Fire.firestore();
+  users = db.collection("users");
+
+  // TEST: Add object to Firestore
+  addUser(name) {
+    users.doc(name).set({
+      first_name: name,
+      last_name: "Smith",
+    });
+  }
+}
--- fouro-application/src/pages/HomePage.js
+++ fouro-application/src/pages/HomePage.js
@@ -1,16 +1,32 @@
-import { Button, Text, View } from 'react-native'
-import React from 'react'
-import AppStyles from '../AppStyles'
+import { Button, Text, View } from "react-native";
+import React from "react";
+import AppStyles from "../AppStyles";
 
-export default function HomePage( { navigation }) {
+// Backend TEST
+import Users from "../../backend/users/Users";
 
-    return (
-        <View style={AppStyles.container}>
-            <Text>home</Text>
-            <Button title="create hug" onPress={() => navigation.navigate('Create Hug')}/>
-            <Button title="add friend" onPress={() => navigation.navigate('Add Friend')}/>
-            <Button title="friend history" onPress={() => navigation.navigate('Friend History')}/>
-            <Button title="friend profile" onPress={() => navigation.navigate('Friend Profile')}/>
-        </View>
-    )
+export default function HomePage({ navigation }) {
+  return (
+    <View style={AppStyles.container}>
+      <Text>home</Text>
+      <Button
+        //title="create hug"
+        title="BACKEND TEST"
+        //onPress={() => navigation.navigate("Create Hug")}
+        onPress={() => Users.addUser("John")} // Backend TEST
+      />
+      <Button
+        title="add friend"
+        onPress={() => navigation.navigate("Add Friend")}
+      />
+      <Button
+        title="friend history"
+        onPress={() => navigation.navigate("Friend History")}
+      />
+      <Button
+        title="friend profile"
+        onPress={() => navigation.navigate("Friend Profile")}
+      />
+    </View>
+  );
 }
