1f9ecbfe41cd699b6787586a0b5783250c86a03f
terry <tzfeng@ucsd.edu>
2020-11-16 12:46:31 -0800

    Removed config.js

--- backend/firebase/config.js
+++
@@ -1,8 +0,0 @@
-// Firebase App (the core Firebase SDK) is always required and
-// must be listed before other Firebase SDKs
-import firebase from "firebase/app";
-
-// Cafe fouro Firebase Configuration
-/* Replace Firebase Config Snippet Here */
-
-export default firebase.initializeApp(firebaseConfig);
