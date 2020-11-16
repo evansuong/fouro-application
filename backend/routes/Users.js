00dba121558536dd988b9c98ab1f0ba82053900d
terry <tzfeng@ucsd.edu>
2020-11-16 13:41:46 -0800

    Users Update

---
+++ backend/routes/Users.js
@@ -0,0 +1,19 @@
+// Users file for Creating, Reading, Updating, and Deleting Users
+// and User Profile Management
+import "firebase/firestore";
+import "firebase/auth";
+import Fire from "../firebase/config";
+// Import timerCorrector to handle trivial timer warnings
+import "../timerCorrector";
+
+// Firestore
+export const db = Fire.firestore();
+export const users = db.collection("users");
+
+// TEST Firestore Function
+export function addUser(username, first, last) {
+  users.doc(username).set({
+    first_name: first,
+    last_name: last,
+  });
+}
