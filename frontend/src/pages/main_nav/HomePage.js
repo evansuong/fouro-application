60ca97724d050f3caf6ba226c06d190620a2d8a2
terry <tzfeng@ucsd.edu>
2020-11-17 00:45:22 -0800

    Firebase init

--- frontend/src/pages/main_nav/HomePage.js
+++ frontend/src/pages/main_nav/HomePage.js
@@ -1,26 +1,30 @@
-import { StyleSheet, Button, Text, View } from 'react-native'
-import React from 'react'
-import AppStyles from '../../AppStyles'
-import Background from 'components/Background';
-import { LinearGradient } from 'expo-linear-gradient';
+import { StyleSheet, Button, Text, View } from "react-native";
+import React from "react";
+import AppStyles from "../../AppStyles";
+import Background from "components/Background";
+import * as Users from "backend/routes/Users";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function HomePage({ navigation }) {
-
-    return (
-      <View style={AppStyles.navPageContainer}>
-          <Text>home</Text>
-          <Button 
-            title="create hug" 
-            onPress={() => navigation.navigate('Create Hug')}
-          />
-          <Button 
-            title="add friend" 
-            onPress={() => navigation.navigate('Add Friend')}
-          />
-          <Button
-            title='launch page'
-            onPress={() => navigation.navigate('Launch Page')}
-          />
-      </View>
-    )
+  return (
+    <View style={AppStyles.navPageContainer}>
+      <Text>home</Text>
+      <Button
+        title="hijack frontend"
+        onPress={() => Users.addUser("evanjserrano", "evan", "serrano")}
+      />
+      <Button
+        title="create hug"
+        onPress={() => navigation.navigate("Create Hug")}
+      />
+      <Button
+        title="add friend"
+        onPress={() => navigation.navigate("Add Friend")}
+      />
+      <Button
+        title="launch page"
+        onPress={() => navigation.navigate("Launch Page")}
+      />
+    </View>
+  );
 }
