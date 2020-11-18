c2b79d72387226d93f7265b08d8c17f6a05cad8e
Evan Serrano <evanjserrano@gmail.com>
2020-11-17 16:23:35 -0800

    Wrote logout and resetPassword

--- backend/routes/account/ManageAccount.js
+++ backend/routes/account/ManageAccount.js
@@ -3,23 +3,30 @@ import Fire from "../firebase/config";
 
 const ManageAccountAPI = {
 
+    user: Fire.auth().currentUser,
+
     logout: function() {
-        
+        Fire.auth().signOut();
     },
 
     resetPassword: function() {
-        
+        var auth = firebase.auth();
+        var emailAddress = user.email;
+
+        auth.sendPasswordResetEmail(emailAddress).then(function() {
+            // Email sent.
+          }).catch(function(error) {
+            // An error happened.
+          });
     },
 
     deleteAccount: function() {
-        var user = Fire.auth().currentUser;
-
         user.delete()
         .then(function() { return true; })                  // user deleted successfully.
         .catch(function(error) { return false; });          // an error happened.
     },
 
     checkLoggedIn: function() {
-        return Fire.auth().currentUser ? true : false;      // currentUser is null if nobody is signed in.
+        return user ? true : false;      // currentUser is null if nobody is signed in.
     }
 }
