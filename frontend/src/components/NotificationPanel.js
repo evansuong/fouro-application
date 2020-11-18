620cebdabab4319636ee5ab15f698a3e4ff096ee
Evan_Suong <evansuong@gmail.com>
2020-11-18 00:15:17 -0800

    notification page updates

--- frontend/src/components/NotificationPanel.js
+++ frontend/src/components/NotificationPanel.js
@@ -1,43 +1,115 @@
-import React, { useState } from 'react'
-import { View, Text, StyleSheet, Animated, Image, TouchableWithoutFeedback } from 'react-native'
+import React, { useState, useRef, useEffect } from 'react'
+import { View, Text, StyleSheet, Animated, Image, TouchableOpacity } from 'react-native'
 
 
-export default function NotificationPanel(props) {
+export default function NotificationPanel({ index, notificationData, clearNotification, isFocused }) {
 
-    const [expanded, setExpanded] = useState(false)
-    const {type, message, image} = props.notificationData;
+    // prop destructuring
+    const { type, message, image, id } = notificationData;
+    // holds if the notification page is focused
+
+    // hold whether or not this panel is expanded or not
+    const [expanded, setExpanded] = useState(false);
+
+    // TODO: implement this to get height of the panel
+    const body = useRef({})
+    
+    // animated value to animate panel expansion and collapse
+    const height = useRef(new Animated.Value(70)).current;
+    const fade = useRef(new Animated.Value(0)).current;
+
+    // auto collapse panels if the user leaves the notification page
+    useEffect(() => {
+        if (!isFocused) {
+            setExpanded(false);
+            collapse();
+        }
+    }, [isFocused])
+
+    // toggle panel expansion on press
+    function handlePress() {
+        setExpanded(!expanded);
+        if(expanded) {
+            collapse();
+        } else {
+            expand();
+        }
+    }
+
+    // expand animation definition
+    function expand() {
+         // Will change fadeAnim value to 1 in 5 seconds
+        Animated.timing(height, {
+            toValue: 130,
+            duration: 200,
+            useNativeDriver: false,
+        }).start();
+        Animated.timing(fade, {
+            toValue: 1,
+            duration: 150,
+            useNativeDriver: false,
+        }).start();
+    }
+
+    // collapse animation definition
+    function collapse() {
+        Animated.timing(height, {
+            toValue: 70,
+            duration: 200,
+            useNativeDriver: false,
+        }).start();
+        Animated.timing(fade, {
+            toValue: 0,
+            duration: 150,
+            useNativeDriver: false,
+        }).start();
+    }
+
 
     return (
-        <TouchableWithoutFeedBack style={styles.hugPanel} onPress={() => setExpanded(!expanded)}>
-
+        <TouchableOpacity style={styles.hugPanel} onPress={handlePress} activeOpacity={1.0}>
+            
             {/* header */}
             <View style={styles.header}>
                 <Text style={styles.text}>{type}</Text>  
+                <TouchableOpacity style={styles.clearBtn} onPress={() => clearNotification(id)}>
+                    <Text style={{color: '#FFFFFF'}}>x</Text>
+                </TouchableOpacity>
             </View>
             
             {/* body */}
-            <View style={styles.body}>
-                <Image style={styles.imageContainer} source={image} />
-                <Text style={styles.text}>{message}</Text>
-            </View>
-            {
-                expanded &&
-                // footer: default hidden
-                <View style={styles.footer}>
-                    <TouchableOpacity>
-                        <View style={{...styles.buttonContainer, backgroundColor: type === 'request' ? '#8677E5' : '#E57777'}}>
-                            <Text style={styles.buttonText}>{type === 'request' ? 'Accept' : 'Catch'}</Text>    
-                        </View>
-                    </TouchableOpacity>
+            <Animated.View
+                style={{
+                    height: height // Bind opacity to animated value
+                }}
+            >
+                <View ref={body}>
+                <View style={styles.body}>
+                    <Image style={styles.imageContainer} source={image} />
+                    <Text style={styles.text}>{message}</Text>
                 </View>
-            }
-        </TouchableWithoutFeedBack>
+                
+                {/* footer button container */}
+                    <View style={styles.footer}>
+                        <TouchableOpacity onPress={() => type === 'request' ? alert('accepted!') : alert('caught hug!')}>
+                            <Animated.View style={{
+                                ...styles.buttonContainer, 
+                                backgroundColor: type === 'request' ? '#8677E5' : '#E57777', 
+                                opacity: fade
+                            }}>
+                                <Text style={styles.buttonText}>{type === 'request' ? 'Accept' : 'Catch'}</Text>    
+                            </Animated.View>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Animated.View>
+        </TouchableOpacity>
     )
 }
         
 const styles = StyleSheet.create({
     hugPanel: {
-        backgroundColor: 'rgba(0, 0, 0, .25)',
+        backgroundColor: 'rgba(0, 0, 0, .55)',
         margin: 5,
         borderRadius: 15,
     },
@@ -45,8 +117,18 @@ const styles = StyleSheet.create({
         backgroundColor: 'rgba(0, 0, 0, .3)',
         borderTopLeftRadius: 15,
         borderTopRightRadius: 15,
-        padding: 5,
-        paddingLeft: 10,
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    clearBtn:{
+        backgroundColor: '#88888888',
+        borderRadius: 100,
+        width: 18,
+        height: 18,
+        display: 'flex',
+        alignItems: 'center',
     },
     body: {
         padding: 15,
--- frontend/src/pages/main_nav/NotificationPage.js
+++ frontend/src/pages/main_nav/NotificationPage.js
@@ -2,48 +2,64 @@ import React, { useState } from 'react';
 import { View, Text, ScrollView, StyleSheet, PickerIOSItem } from 'react-native';
 import NotificationPanel from '../../components/NotificationPanel';
 
+import { useFocusEffect } from '@react-navigation/native';
+
+// temorary test data to simulate backend notification data 
+const pic = require('../../../assets/profilePic.jpg')
+   
+function buildTestData(type, msg, img, id) {
+    return {
+        type: type,
+        message: msg,
+        image: img,
+        id: id
+    }
+}
+
+
+const testData = [
+    buildTestData('request', 'Alex Chow', require('../../../assets/profilePic.jpg'), 0),
+    buildTestData('hug', 'Evan Suong', require('../../../assets/profilePic.jpg'), 1),
+    buildTestData('hug', 'Yixuan Zhou', require('../../../assets/profilePic.jpg'), 2),
+    buildTestData('request', 'Tyus Liu', require('../../../assets/profilePic.jpg'), 3),
+    buildTestData('request', 'Vicki Chen', require('../../../assets/profilePic.jpg'), 4),
+]
 
 export default function NotificationPage({ navigation }) {
 
-    // const [notifications, setNotifications] = useState(testData)
-    const pic = require('../../../assets/profilePic.jpg')
+    // stores whether the user is on this page (true) or not (false)
+    const [isFocused, setIsFocused] = useState(false)
+    const [notifications, setNotifications] = useState(testData ? testData : {})
 
-    const testData = [
-        buildTestData('request', 'yixuaiend request', require('../../../assets/profilePic.jpg')),
-        buildTestData('hug', 'yixuas sent you a hug', require('../../../assets/profilePic.jpg')),
-        buildTestData('hug', 'yixuas sent you a hug', require('../../../assets/profilePic.jpg')),
-        buildTestData('request', 'yixuan has sent you a friend request', require('../../../assets/profilePic.jpg')),
-        buildTestData('request', 'yixuan has sent you a friend request', require('../../../assets/profilePic.jpg')),
-    ]
+    // check whether the user is on the page (true) or navigates away from the page (false)
+    useFocusEffect(() => {
+        setIsFocused(true)
+        return () => {
+           setIsFocused(false)
+        }
+    }, []);  
+
+    function clearNotification(id) {
+        const newList = notifications.filter((item) => item.id !== id);
+        setNotifications(newList)
+    }
 
     
-    function buildTestData(type, msg, img) {
-        return {
-            type: type,
-            message: msg,
-            image: img,
-        }
-    }
-
-
-    const scrollProps = {
-        showsVerticalScrollIndicator: false,
-
-    }
-
+    // map every notification entry to a notification panel element 
     return (
         <View style={styles.notificationList}>
-            <ScrollView {...scrollProps}>
-                {testData.map(data => (
-                    <NotificationPanel notificationData={data} />
+            <ScrollView scrollProps={{ showsVerticalScrollIndicator: false }}>
+                {notifications.map((data, index) => (
+                    <NotificationPanel key={index} index={index} notificationData={data} isFocused={isFocused} clearNotification={clearNotification} />
                 ))}
             </ScrollView>
         </View>
     )
-}
+} 
 
+// notification list styles
 const styles = StyleSheet.create({
     notificationList: {
-        margin: 5,
+        marginHorizontal: 5,
     }
 })
