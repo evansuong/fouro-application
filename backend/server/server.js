5d2fdb6e1c58917f06d57d78cd58e665b6dd5d36
terry <tzfeng@ucsd.edu>
2020-11-25 16:44:39 -0800

    Initialized Routes and Server

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,1 +1,38 @@
-//temp file
+// Backend http server for managing front end calls and
+// connecting to firebase
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+// Including Routes
+const account = require("../routes/accountRoutes");
+const corkboard = require("../routes/corkboardRoutes");
+const friends = require("../routes/friendsRoutes");
+const hugs = require("../routes/hugsRoutes");
+const notifications = require("../routes/notificationsRoutes");
+const users = require("../routes/usersRoutes");
+
+// Server Setup
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+app.use(cors());
+
+// Attaching Routes
+app.use("/account", account);
+app.use("/corkboard", corkboard);
+app.use("/friends", friends);
+app.use("/hugs", hugs);
+app.use("/notifications", notifications);
+app.use("/users", users);
+
+// Server
+const http = require("http");
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server running at ${port}!`);
+});
