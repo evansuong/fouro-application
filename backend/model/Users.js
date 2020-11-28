bcb842962b1967c7b22a11eedd77c0987c9a048a
Evan Serrano <evanjserrano@gmail.com>
2020-11-28 14:48:36 -0800

    Moved authentication api to frontend

--- backend/model/Users.js
+++ backend/model/Users.js
@@ -136,7 +136,14 @@ const UsersAPI = {
       .set({
         profile_pic:  storageRef
       });
-  }
+  },
+
+  /*usernameTaken: async function(username) {
+    console.log('user input: ', username);
+    const response = users.where('username', '==', username);
+    const query = await response.get();
+    return !query.empty;
+  }*/
 };
  
 const HugCountAPI = {
--- backend/model/account/ManageAccount.js
+++ backend/model/account/ManageAccount.js
@@ -52,20 +52,6 @@ const ManageAccountAPI = {
   checkLoggedIn: function () {
     return user ? true : false; // currentUser is null if nobody is signed in.
   },
-/*
-  emailTaken: async function(email) {
-    const response = users.where('email', '==', email);
-    const query = await response.get();
-    return !query.empty;
-  },
-
-  */
-  usernameTaken: async function(username) {
-    console.log('user input: ', username);
-    const response = users.where('username', '==', username);
-    const query = await response.get();
-    return !query.empty;
-  }
   
 };
 
