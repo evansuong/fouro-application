a898fd3fac1968f2bb2f8956db88243113105539
Rickesh Khilnani <rikhilna@ucsd.edu>
2020-12-04 21:09:22 -0800

    removed initial route

--- frontend/App.js
+++ frontend/App.js
@@ -49,7 +49,6 @@ export default function App() {
           <NavigationContainer>
             <Stack.Navigator 
               style={styles.appContainer}
-              initialRouteName='Signup Page'
               /** 
                * comment out the line below when you need the header for 
               /* going back to the previous screen. Leave it to see what 
