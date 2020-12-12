9550d0abd8680a3871a794bce393121f747cc2d6
terry <tzfeng@ucsd.edu>
2020-12-12 00:31:36 -0800

    commit

--- backend/test/deleteAllAuthUsers.js
+++ backend/test/deleteAllAuthUsers.js
@@ -1,20 +1,63 @@
-const listAllUsers = (nextPageToken) => {
-  // List batch of users, 1000 at a time.
-  admin
-    .auth()
-    .listUsers(1000, nextPageToken)
-    .then((listUsersResult) => {
-      listUsersResult.users.forEach((userRecord) => {
-        console.log("user", userRecord.toJSON());
+const admin = require("../firebase/admin");
+async function main() {
+  let users = [];
+
+  const listAllUsers = async function (nextPageToken) {
+    // List batch of users, 1000 at a time.
+    await admin
+      .auth()
+      .listUsers(1000, nextPageToken)
+      .then((listUsersResult) => {
+        listUsersResult.users.forEach((userRecord) => {
+          //console.log("user", userRecord.toJSON().uid);
+          users = [...users, userRecord.toJSON().uid];
+        });
+        if (listUsersResult.pageToken) {
+          // List next batch of users.
+          listAllUsers(listUsersResult.pageToken);
+        }
+      })
+      .catch((error) => {
+        console.log("Error listing users:", error);
       });
-      if (listUsersResult.pageToken) {
-        // List next batch of users.
-        listAllUsers(listUsersResult.pageToken);
-      }
+  };
+
+  // Start listing users from the beginning, 1000 at a time.
+  await listAllUsers();
+  //console.log(users);
+
+  await admin
+    .auth()
+    .deleteUsers(users)
+    .then((deleteUsersResult) => {
+      console.log(
+        `Successfully deleted ${deleteUsersResult.successCount} users`
+      );
+      console.log(`Failed to delete ${deleteUsersResult.failureCount} users`);
+      deleteUsersResult.errors.forEach((err) => {
+        console.log(err.error.toJSON());
+      });
     })
     .catch((error) => {
-      console.log("Error listing users:", error);
+      console.log("Error deleting users:", error);
     });
-};
-// Start listing users from the beginning, 1000 at a time.
-listAllUsers();
+}
+function something() {
+  const admin = require("../firebase/admin");
+  admin.auth().createUser({
+    email: "evanjserrano@gmail.com",
+    password: "password",
+    uid: "evanjserrano",
+  });
+}
+
+//something();
+
+const Users = require("../model/Users");
+
+Users.UsersAPI.createNewUser(
+  "evanjserrano",
+  "evanjserrano1",
+  "Evan",
+  "Serrano"
+);
