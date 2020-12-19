2087d66427a6d7ec8567c217e3e9a62d29713d9b
terry <tzfeng@ucsd.edu>
2020-12-18 17:17:12 -0800

    Cloud Functions are connected to server and write log statements

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,8 +1,12 @@
 // Backend http server for managing front end calls and
 // connecting to firebase
+"use strict";
+
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const cron = require("node-cron");
+const pino = require("pino");
 
 // Including Routes
 const account = require("../routes/accountRoutes");
@@ -12,6 +16,12 @@ const hugs = require("../routes/hugsRoutes");
 const notifications = require("../routes/notificationsRoutes");
 const users = require("../routes/usersRoutes");
 
+// Server automated Cloud Functions
+const CloudFunctionsAPI = require("../model/CloudFunctions");
+
+// Logging Cloud Functions
+const logger = pino({ level: process.nextTick.LOG_LEVEL || "warn" });
+
 // Server Setup
 app.use(
   express.urlencoded({
@@ -20,6 +30,7 @@ app.use(
 );
 app.use(express.json());
 app.use(cors());
+app.use(pino);
 
 // Attaching Routes
 app.use("/account", account);
@@ -28,6 +39,13 @@ app.use("/friends", friends);
 app.use("/hugs", hugs);
 app.use("/notifications", notifications);
 app.use("/users", users);
+
+// Cron - Cloud Function usage
+cron.schedule("0 0 0 * * *", function () {
+  // Run at 00:00:00
+  logger.warn("It's 00:00. Resetting all user hug counts :)");
+  CloudFunctionsAPI.resetUserHugCounts();
+});
 
 // Server
 const port = 3000;
